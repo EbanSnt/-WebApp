c48e19bdbcbe43abe9c2e0c8136405baec68bbae
adrian411997 <adrian.mart41197@gmail.com>
2023-05-17 20:35:17 -0300

    Add Model Prestamo_libro

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Prestamo_libro(models.Model):
+    socio = models.ForeignKey(Socio, on_delete=models.CASCADE)
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    fecha_prestamo = models.DateField()
+    fecha_devolucion = models.DateField()
