09e109c2aaa41f091564e6370dabc9ee7805156c
EbanSnt <negociosebansnt@gmail.com>
2023-05-17 20:02:20 -0300

    Update models.py

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Socio(models.Model):
+    
+    nombre = models.CharField()
+    apellido = models.CharField()
+    fecha_nacimiento = models.DateField()
+    activo = models.BooleanField(default=True)
