ff275e4f3e05e784c58999048ef0466ad160a6d1
Gaston <gaston010.gv@gmail.com>
2023-05-18 17:37:54 -0300

    ..

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -3,6 +3,18 @@ from django.db import models
 # Create your models here.
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 class Libro(models.Model):
     titulo = models.CharField(max_length=100)
     descripcion  = models.TextField()
