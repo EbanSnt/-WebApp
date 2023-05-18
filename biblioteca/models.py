fdf37be73d01552f6fe3f531024648e7debc0f64
RS4400 <133908306+RS4400@users.noreply.github.com>
2023-05-18 15:48:55 -0300

    Update models.py

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    nacionalidad = models.CharField(max_length=30)
+    activo = models.BooleanField(default=True)
