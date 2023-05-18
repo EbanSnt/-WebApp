b270b14b6bb7a69c206b4c57d852b139071a0274
RS4400 <133908306+RS4400@users.noreply.github.com>
2023-05-18 17:47:49 -0300

    Update models.py
    .

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Empleado(models.Model):
-
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     numero_legajo = models.CharField(max_length=30)
