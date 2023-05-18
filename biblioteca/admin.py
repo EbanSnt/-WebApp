00029bb10c11676a890990fbd347bc0bef3f3d89
Gaston <gaston010.gv@gmail.com>
2023-05-18 12:47:20 -0300

    Admin Autor

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Autor, Libro
+
+#regisro Admin autor rama SC5S2-21 from SC5S2-15
+class AutorAdmin(admin.ModelAdmin):
+    #Lista por donde como se muestra en el django admin
+    list_display = ('nombre', 'apellido', 'nacionalidad', 'activo' )
+    #filtro por el cual se puede buscar en el django admin
+    list_search =( 'nombre', 'apellido' )
+    #filtro por el cual se puede filtrar en el django admin
+    list_filtet = ('activo', 'nacionalidad')
+
+admin.site.register(Autor, AutorAdmin)
+
+
