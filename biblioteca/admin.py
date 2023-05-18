329c0aa81830403137616978a5a0bd696d546256
EbanSnt <negociosebansnt@gmail.com>
2023-05-18 18:29:41 -0300

    models Empleado

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Empleado
+# Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("nombre", "apellido", "numero_legajo","activo",)
+  list_filter = ('activo',)
+  list_search = ('nombre', 'apellido',)
+
+admin.site.register(Empleado, MemberAdmin)
