0b0ce42ddc56ab3bdf2e5175edf3887afea06b0a
Gaston <gaston010.gv@gmail.com>
2023-05-18 18:18:27 -0300

    merger from main

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 # Create your models here.
 
-# Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    nacionalidad = models.CharField(max_length=30)
+    activo = models.BooleanField(default=True)
+
+    
+class Libro(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion  = models.TextField()
+    isbn = models.CharField(max_length=13, unique=True, null=False)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
 class Autor(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
@@ -17,7 +33,6 @@ class Empleado(models.Model):
     
 
 class Socio(models.Model):
-
     nombre = models.CharField()
     apellido = models.CharField()
     fecha_nacimiento = models.DateField()
