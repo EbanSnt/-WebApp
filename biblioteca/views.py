dbbd4b4033cfa1c3d4eec46e565c3a69d9dd66b6
Gaston <gaston010.gv@gmail.com>
2023-05-18 19:52:23 -0300

    SC5S2-25

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
+
+def registrar_empleado(request):
+    form = EmpleadoForm()
+    if request.method =="POST":
+        form = EmpleadoForm(request.POST)
+        if form.is_valid():
+
+            empl = Empleado(
+                nombre = form.cleaned_data["nombre"],
+                apellido = form.cleaned_data["apellido"],
+                numero_legajo = form.cleaned_data["numero_legajo"],
+                activo = form.cleaned_data["activo"]
+            )
+            empl.save()
+            return redirect("/empleado_lista/")
+    else:   
+        return redirect("/empleado_lista/")
+    context = { "form": form }
+
+    return render(request, "empleado_form.html", context)
