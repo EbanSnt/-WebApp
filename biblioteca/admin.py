ec111835ba43229ba12a96070069c7de8311f224
EbanSnt <negociosebansnt@gmail.com>
2023-05-18 17:59:53 -0300

    Update admin.py

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Empleado
 # Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("nombre", "apellido", "numero_legajo","activo",)
+  list_filter = ('activo',)
+  list_search = ('nombre', 'apellido',)
+
+admin.site.register(Empleado, MemberAdmin)
