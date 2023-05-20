ea43883093431929180eecad7928250efa078d6d
adrian411997 <92401723+adrian411997@users.noreply.github.com>
2023-05-19 21:39:20 -0300

    FixedUrlsandStyles

--- biblioteca/forms.py
+++ biblioteca/forms.py
@@ -6,3 +6,9 @@ class EmpleadoForm(forms.ModelForm):
     class Meta():
         model = Empleado
         fields = '__all__'
+        widgets={"nombre":forms.TextInput(attrs={"class":"form-control my-3"}),
+                 "apellido":forms.TextInput(attrs={"class":"form-control my-3"}),
+                 "numero_legajo":forms.TextInput(attrs={"class":"form-control my-3","type":"number"}),
+                 "activo":forms.CheckboxInput(attrs={"class":"form-check-input my-2"})
+                 }
+ 
