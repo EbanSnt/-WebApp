dc97ac91d242512ae332f1e178f27159077c8096
EbanSnt <negociosebansnt@gmail.com>
2023-05-28 19:31:38 -0300

    Update urls.py

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path("empleados/<int:id>/modificar/",actualizar_empleado, name="actualizar_empleado"),
     path("socios/", socio_lista, name="socio_lista"),
     path("socios/<int:id>/modificar/", actualizar_socio, name="actualizar_socio"),
-    path("autores/nuevo/",registrar_autor,name="registrar_autor")
-    #path("autores", registrar_autor, name="registrar_autor")
+    #path("autores/<int:id>/modificar/",modificar_autor,name="modificar_autor"),
+    #path("autores/nuevo/",registrar_autor,name="registrar_autor"),
+    #path("autores/",autores_lista, name="autores_lista"),
+    path("autores/<int:id>/activar,",activo_cambiar_autor,name="autor_lista"),
+    #path("autores/<int:id>/desactivar,",desactivar_autor,name="desactivar_autor")
+    path("autores", registrar_autor, name="registrar_autor"),
 ]
