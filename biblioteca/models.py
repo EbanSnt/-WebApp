bddd5730c14e9aeafbfdd333bc2943751bc81ff2
adrian411997 <adrian.mart41197@gmail.com>
2023-05-30 20:39:03 -0300

    Sprint3AllTasks

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -20,7 +20,7 @@ class Libro(models.Model):
     activo = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.nombre
+        return self.titulo
 
 class Empleado(models.Model):
     nombre = models.CharField(max_length=30)
